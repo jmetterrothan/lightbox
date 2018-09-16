c80ec856d301060e1dd8f6b9202805c26fbc5116
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2018-09-17 00:03:12 +0200

    LbException added name

--- src/LbException.js
+++ src/LbException.js
@@ -1,7 +1,9 @@
+'use strict';
+
 class LbException extends Error {
     constructor(...args) {
         super(...args);
-
+        this.name = 'LbException';
         if (Error.captureStackTrace) {
             Error.captureStackTrace(this, LbException);
         }
