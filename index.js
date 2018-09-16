61106fc3aa9a18213f473df790d5e4c19d087fc8
Metter-Rothan Jérémie <jmetterrothan@gmail.com>
2018-09-16 10:33:50 +0200

    Linter config added

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import Lightbox from './src/Lightbox';
+import LbElement from './src/LbElement';
+import LbException from './src/LbException';
 
 export default Lightbox;
 
 export {
-
+    LbElement,
+    LbException,
 };
